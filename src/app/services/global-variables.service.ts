cc696512ca64ece0a96389c70bf8258c3d2c8d59
dudushy <eduardotalarico1337@gmail.com>
2022-09-03 16:33:59 -0300

    log constructor

--- src/app/services/global-variables.service.ts
+++ src/app/services/global-variables.service.ts
@@ -5,5 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class GlobalVariablesService {
 
-  constructor() { }
+  constructor() {
+    console.log('[GlobalVariablesService#constructor]');
+  }
 }
--- src/app/services/test.service.ts
+++ src/app/services/test.service.ts
@@ -5,5 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class TestService {
 
-  constructor() { }
+  constructor() {
+    console.log('[TestService#constructor]');
+  }
 }
