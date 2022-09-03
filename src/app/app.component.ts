187209c440809d2220acfc39ed1cec4bb02dac39
dudushy <eduardotalarico1337@gmail.com>
2022-09-03 15:58:21 -0300

    Move project files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fun';
+
+  constructor(
+    private router: Router
+  ) { }
+
+  redirectTo(url:string) {
+    this.router.navigate(`/${url}`);
+  }
 }
